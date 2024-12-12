228cfe5ef780aa0717b7248cf60b80d0ca3d0f4c
Jatin Joshi <119153089+Gobu105@users.noreply.github.com>
2024-12-12 22:26:20 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php include'inc/header.php'?>
+    <div class="bg-image">
+    </div>
+    <div class="bg-text">
+        <h1>Skill-Stock</h1>
+        <h2>"From Basics to Brilliance in Trading."</h2>
+    </div>
 
 <?php include'inc/footer.php'?>
